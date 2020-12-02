426e701409866b201b6018c7da0799c43c6b3c6b
inapifer94 <ignaciopierafdez@gmail.com>
2020-12-02 18:16:48 +0100

    Giving a description to the linear activation, changing the parameter A for W, and the X for A.

--- main_functions.py
+++ main_functions.py
@@ -2,5 +2,11 @@
 
 import numpy as np
 
-def linear_activation(A, X, b):
-    return np.matmul(A,X) + b
+def linear_activation(W, A, b):
+    
+    """
+    Receives as parameters the weights of the network W,
+    the activation function X and the bias.
+    """
+
+    return np.matmul(W, A) + b
