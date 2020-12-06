ecd91bc0ed52fc9f46f87ff565a91a1f4f64d829
inapifer94 <ignaciopierafdez@gmail.com>
2020-12-06 13:16:10 +0100

    I have added in the main_functions script the derivatives of the network for the backpropagation algorithm and I have tried them in the Jupyter notebook. I also modified the functions activation_functions and derivative_activation to raise an Exception error if the activation name does not correspond with some of the allowed ones.

--- main_functions.py
+++ main_functions.py
@@ -32,22 +32,27 @@ def derivative_sigmoid(Z):
     (Scalars)"""
     return sigmoid(Z)*(1-sigmoid(Z))
 
-def derivative_cost_logistic(Y_hat, Y):
-    """It returns the value of the derivatives of the cost function
-    respect to Y_hat, which is the same as A[L], i.e., the activation
-    of the last layer considering we use the logistic regression
-    function."""
-    
-    dA_L = -Y/Y_hat + (1-Y)/(1-Y_hat)
-
-    return dA_L
 
 def activation_functions(Z, activation_name):
     """Doing the activation function of relu and sigmoid"""
     if activation_name.lower() == 'relu':
         return relu(Z)
-    if activation_name.lower() == 'sigmoid':
+    elif activation_name.lower() == 'sigmoid':
         return sigmoid(Z)
+    else:
+        raise Exception("The only activation functions available\
+                         are relu and sigmoid")
+
+
+def derivative_activation(Z, activation_name):
+    """Doing the derivative of the activation functions."""
+    if activation_name.lower() == 'relu':
+        return derivative_relu(Z)
+    elif activation_name.lower() == 'sigmoid':
+        return derivative_sigmoid(Z)
+    else:
+        raise Exception("The only activation functions\
+                         available are relu ans sigmoid")
 
 def forward_activation(W, A_prev, b, activation_name):
     """Doing the linear activation and the forward activation in
@@ -56,3 +61,45 @@ def forward_activation(W, A_prev, b, activation_name):
     A = activation_functions(Z, activation_name)
 
     return A
+
+
+def derivative_cost_logistic(Y_hat, Y):
+    """It returns the value of the derivatives of the cost function
+    respect to Y_hat, which is the same as A[L], i.e., the activation
+    of the last layer considering we use the logistic regression
+    function."""
+
+    dA_L = -Y/Y_hat + (1-Y)/(1-Y_hat)
+
+    return dA_L
+
+def derivatives_Z(dA, Z, activation_name):
+    """It returns the value of the derivative of the cost function
+    with respect to Z of certain layer."""
+    assert dA.shape == Z.shape, "Dimensions of dA and Z must be the same"
+    # I calculate the derivative of the activation function in this
+    #layer
+    d_activation = derivative_activation(Z, activation_name)
+
+    # Now I make the (non-matricial) product of the derivatives
+    # of the activation and dA.
+    return dA*d_activation
+
+def derivatives_W(dZ, A_prev):
+    """Returns the derivatives of the weights in the layer l"""
+    assert dZ.shape[1] == A_prev.shape[1], \
+    "Number of samples must be the same in dZ and A_prev"
+    m = dZ.shape[1]
+    dW = 1/m * np.matmul(dZ, A_prev.T)
+    return dW
+def derivatives_b(dZ):
+    """Returns the derivatives of the biases in certain layers."""
+    m = dZ.shape[1]
+    db = 1/m * np.sum(dZ, axis=1, keepdims=True)
+    return db
+
+def derivatives_dA_prev(W, dZ):
+    """Returns the derivatives of the activations of the
+    before this one."""
+    dA_prev = np.matmul(W.T, dZ)
+    return dA_prev
