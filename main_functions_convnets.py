0dd9a00882825ef2d453212adea36a6944f4f356
inapifer94 <ignaciopierafdez@gmail.com>
2020-12-15 13:25:33 +0100

    I have added a padding function and I have started the convolutional operation that operates throughout all the samples of the network. I've got the dimensions of the output and now I need to do the convolution operation in each sample.

--- main_functions_convnets.py
+++ main_functions_convnets.py
@@ -6,12 +6,12 @@ def basic_convolution(portion, filter):
      """It makes the basic operation of convolution in 2D in which
      it multiplies the matrixes and sums the whole result."""
 
+     #First, we make sure that the dimensions fit.
      portion_dims = portion.shape
      filter_dims = filter.shape
 
      assert portion_dims == filter_dims, "The number dimensions of the portion \
      and filter must be the same and the same shape"
-
 
      conv_operation = (portion * filter).sum()
      return conv_operation
