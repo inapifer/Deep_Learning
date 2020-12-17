db881122d331b4bdd4ba545f535057c375469e4c
inapifer <ignaciopierafdez@gmail.com>
2020-12-17 20:05:50 +0100

    I cleaned boxes of the jupyter notebook and I added to the main_functions_convnets script the functions get_n_H and get_n_W just to make cleaner the code for getting the height and width of the output.

--- main_functions_convnets.py
+++ main_functions_convnets.py
@@ -2,6 +2,24 @@
 
 import numpy as np
 
+def get_n_H(n_H_prev, f, pad, stride):
+    """Returns the height of the next layer after considering the height of the
+    previous one, the size of the filter f, the pad applied to the input and the
+    stride."""
+
+    n_H = int((n_H_prev + 2*pad - f)/stride + 1)
+    return n_H
+
+def get_n_W(n_W_prev, f, pad, stride):
+    """Returns the height of the next layer after considering the height of the
+    previous one, the size of the filter f, the pad applied to the input and the
+    stride."""
+    
+    n_W = nt((n_W_prev + 2*pad - f)/stride + 1)
+    return n_W
+
+
+
 def basic_convolution(portion, filter):
      """It makes the basic operation of convolution in 2D in which
      it multiplies the matrixes and sums the whole result."""
